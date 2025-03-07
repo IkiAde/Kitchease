d03f4496493dbd12cc7a68a2eef7a341d96e27b5
ikimath <ikimathadeoye@gmail.com>
2025-03-07 16:52:27 +0100

    initialisation du projet

---
+++ KitchEase/src/main/java/com/example/KitchEase/KitchEaseApplication.java
@@ -0,0 +1,13 @@
+package com.example.KitchEase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KitchEaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KitchEaseApplication.class, args);
+	}
+
+}
