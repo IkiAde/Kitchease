7269edf87a90f4cab34ef516379570b78aa78db8
elisabethbercy <bercystephaniee@gmail.com>
2025-05-23 09:53:15 +0200

    postgresql error correction!

--- gestKitchease/src/main/java/GestionKitchease/gestKitchease/controller/WebConfig.java
+++ gestKitchease/src/main/java/GestionKitchease/gestKitchease/controller/WebConfig.java
@@ -1,3 +1,4 @@
+package GestionKitchease.gestKitchease.controller;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
